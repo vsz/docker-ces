e5e840b244ad2f759125f58deec4a0c81c501128
vsz <vsz@certi.org.br>
2020-12-04 08:59:57 -0300

    Primeiros exemplos.

---
+++ docker_examples/ex4/src/hello.py
@@ -0,0 +1,8 @@
+import requests
+import redis
+
+def main():
+    print('Hello World!')
+
+if __name__ == '__main__':
+    main()
---
+++ docker_examples/ex5/src/print_tables.py
@@ -0,0 +1,9 @@
+import mysql-connector
+
+def main():
+    while True:
+        print('DB Tables')
+
+
+if __name__ == '__main__':
+    main()
